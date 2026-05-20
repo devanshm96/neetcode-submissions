8e878219b62e2709b489c07901dd57110b99b0d9
Devansh Mudgal <72990407+devanshm96@users.noreply.github.com>
2026-05-20 23:11:46 +0530

    Bulk sync: 12 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.py
@@ -0,0 +1,22 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        # for i in range(len(nums)):
+        #     for j in range(i+1, len(nums)):
+        #         if nums[i]==nums[j]:
+        #             return True
+        # return False
+
+        # nums.sort()
+        # for i in range(len(nums)-1):
+        #     if nums[i] == nums[i+1]:
+        #         return True
+        # return False
+         
+        # hset=set()
+        # for num in nums:
+        #     if num in hset:
+        #         return True
+        #     hset.add(num)
+        # return False
+
+        return len(set(nums)) < len(nums)
---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # hmap={}
+
+        # for i, n in enumerate(nums):
+        #     diff = target - n
+
+        #     if diff in hmap:
+        #         return [hmap[diff], i]
+            
+        #     hmap[n] = i
+        
+        # ans = [0]*2
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i]+nums[j] == target:
+                    # ans[0] = i
+                    # ans[1] = j
+                    return [i,j]
+        return []
